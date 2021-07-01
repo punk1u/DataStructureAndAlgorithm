ea6683fd693cb84c5073b0b56e980774cddbb33a
punk1u <punk1u@protonmail.com>
2021-07-01 12:59:45 +0800

    commit leetcode88 solution

---
+++ leetcode/src/tech/punklu/leetcode/LC88.java
@@ -0,0 +1,64 @@
+package tech.punklu.leetcode;
+
+/**
+ * 给你两个有序整数数组nums1 和 nums2，请你将 nums2 合并到nums1中，使 nums1 成为一个有序数组。
+ *
+ * 初始化nums1 和 nums2 的元素数量分别为m 和 n 。你可以假设nums1 的空间大小等于m + n，这样它就有足够的空间保存来自 nums2 的元素。
+ *
+ *
+ * 示例 1：
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ *
+ * 示例 2：
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ *
+ *
+ * 提示：
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -10^9 <= nums1[i], nums2[i] <= 10^9
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LC88 {
+
+    /**
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：38.6 MB, 在所有 Java 提交中击败了51.79%的用户
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int n1Index = m - 1;
+        int n2Index = n - 1;
+        int mergeIndex = (m + n) - 1;
+        while (mergeIndex >= 0){
+            int n1 = n1Index >= 0 ?nums1[n1Index]:Integer.MIN_VALUE;
+            int n2 = n2Index >= 0 ?nums2[n2Index]:Integer.MIN_VALUE;
+            if (n1 >= n2){
+                nums1[mergeIndex--] = n1;
+                n1Index -=1;
+            }else {
+                nums1[mergeIndex--] = n2;
+                n2Index -=1;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        merge(nums1,3,nums2,3);
+        for (int i = 0; i < nums1.length; i++) {
+            System.out.println(nums1[i]);
+        }
+    }
+}

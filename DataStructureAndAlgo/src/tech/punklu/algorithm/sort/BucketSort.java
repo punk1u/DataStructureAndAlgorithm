fe7b272c366564ec1d2f771b70ea173012d6b645
punk1u <punk1u@protonmail.com>
2021-07-10 12:42:34 +0800

    commit bucket sort and couting sort code and comment

---
+++ DataStructureAndAlgo/src/tech/punklu/algorithm/sort/BucketSort.java
@@ -0,0 +1,143 @@
+package tech.punklu.algorithm.sort;
+
+/**
+ * 桶排序
+ * 时间复杂度O(n)
+ * 桶排序对要排序数据的要求是非常苛刻的。首先，要排序的数据需要很容易就能划分成m个桶，
+ * 并且，桶与桶之间有着天然的大小顺序。这样每个桶内的数据都排序完之后，
+ * 桶与桶之间的数据不需要再进行排序。其次，数据在各个桶之间的分布是比较均匀的。
+ * 如果数据经过桶的划分之后，有些桶里的数据非常多，有些非常少，很不平均，
+ * 那桶内数据排序的时间复杂度就不是常量级了。在极端情况下，
+ * 如果数据都被划分到一个桶里，那就退化为O(nlogn)的排序算法了。
+ */
+public class BucketSort {
+
+    /**
+     * 桶排序
+     *
+     * @param arr 数组
+     * @param bucketSize 桶容量
+     */
+    public static void bucketSort(int[] arr, int bucketSize) {
+        if (arr.length < 2) {
+            return;
+        }
+
+        // 数组最小值
+        int minValue = arr[0];
+        // 数组最大值
+        int maxValue = arr[1];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minValue) {
+                minValue = arr[i];
+            } else if (arr[i] > maxValue) {
+                maxValue = arr[i];
+            }
+        }
+
+        // 桶数量
+        int bucketCount = (maxValue - minValue) / bucketSize + 1;
+        int[][] buckets = new int[bucketCount][bucketSize];
+        int[] indexArr = new int[bucketCount];
+
+        // 将数组中值分配到各个桶里
+        for (int i = 0; i < arr.length; i++) {
+            int bucketIndex = (arr[i] - minValue) / bucketSize;
+            if (indexArr[bucketIndex] == buckets[bucketIndex].length) {
+                ensureCapacity(buckets, bucketIndex);
+            }
+            buckets[bucketIndex][indexArr[bucketIndex]++] = arr[i];
+        }
+
+        // 对每个桶进行排序，这里使用了快速排序
+        int k = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            if (indexArr[i] == 0) {
+                continue;
+            }
+            quickSortC(buckets[i], 0, indexArr[i] - 1);
+            for (int j = 0; j < indexArr[i]; j++) {
+                arr[k++] = buckets[i][j];
+            }
+        }
+    }
+
+    /**
+     * 数组扩容
+     *
+     * @param buckets
+     * @param bucketIndex
+     */
+    private static void ensureCapacity(int[][] buckets, int bucketIndex) {
+        int[] tempArr = buckets[bucketIndex];
+        int[] newArr = new int[tempArr.length * 2];
+        for (int j = 0; j < tempArr.length; j++) {
+            newArr[j] = tempArr[j];
+        }
+        buckets[bucketIndex] = newArr;
+    }
+
+    /**
+     * 快速排序递归函数
+     *
+     * @param arr
+     * @param p
+     * @param r
+     */
+    private static void quickSortC(int[] arr, int p, int r) {
+        if (p >= r) {
+            return;
+        }
+
+        int q = partition(arr, p, r);
+        quickSortC(arr, p, q - 1);
+        quickSortC(arr, q + 1, r);
+    }
+
+    /**
+     * 分区函数
+     *
+     * @param arr
+     * @param p
+     * @param r
+     * @return 分区点位置
+     */
+    private static int partition(int[] arr, int p, int r) {
+        int pivot = arr[r];
+        int i = p;
+        for (int j = p; j < r; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+        }
+
+        swap(arr, i, r);
+        return i;
+    }
+
+    /**
+     * 交换
+     *
+     * @param arr
+     * @param i
+     * @param j
+     */
+    private static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {6,5,4,3,2,1};
+        bucketSort(a,6);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}

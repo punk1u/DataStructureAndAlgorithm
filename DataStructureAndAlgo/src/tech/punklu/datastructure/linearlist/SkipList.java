ec49e9747c917473c7917649c6365edce7e818e6
punk1u <punk1u@protonmail.com>
2021-07-12 22:36:13 +0800

    commit skiplist data structure code and comment

--- DataStructureAndAlgo/src/tech/punklu/datastructure/linearlist/SkipList.java
+++ DataStructureAndAlgo/src/tech/punklu/datastructure/linearlist/SkipList.java
@@ -1,135 +1,171 @@
 package tech.punklu.datastructure.linearlist;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Random;
 
 /**
  * 跳表 数据结构
  */
 public class SkipList {
-    /**
-     * 最大层数
-     */
-    private static int DEFAULT_MAX_LEVEL = 32;
-    /**
-     * 随机层数概率，也就是随机出的层数，在 第1层以上(不包括第一层)的概率，层数不超过maxLevel，层数的起始号为1
-     */
-    private static double DEFAULT_P_FACTOR = 0.25;
+
+    private static final int MAX_LEVEL = 16;
+    private int levelCount = 1;
 
     /**
-     * 头节点
+     * 带头链表
      */
-    Node head = new Node(null,DEFAULT_MAX_LEVEL);
+    private Node head = new Node(MAX_LEVEL);
+    private Random r = new Random();
 
-    /**
-     * 表示当前nodes的实际层数，它从1开始
-     */
-    int currentLevel = 1;
-
-
-    public SkipList() {
-    }
-
-    public boolean search(int target) {
-        Node searchNode = head;
-        for (int i = currentLevel-1; i >=0; i--) {
-            searchNode = findClosest(searchNode, i, target);
-            if (searchNode.next[i]!=null && searchNode.next[i].value == target){
-                return true;
+    public Node find(int value) {
+        Node p = head;
+        // 从最大层开始查找，找到前一节点，通过--i，移动到下层再开始查找
+        for (int i = levelCount - 1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                // 找到前一节点
+                p = p.forwards[i];
             }
         }
-        return false;
+
+        if (p.forwards[0] != null && p.forwards[0].data == value) {
+            return p.forwards[0];
+        } else {
+            return null;
+        }
     }
 
     /**
-     * 添加元素
-     * @param num
+     * 插入元素
+     * @param value 值
      */
-    public void add(int num) {
-        int level = randomLevel();
-        Node updateNode = head;
-        Node newNode = new Node(num,level);
-        // 计算出当前num 索引的实际层数，从该层开始添加索引
-        for (int i = currentLevel-1; i>=0; i--) {
-            //找到本层最近离num最近的list
-            updateNode = findClosest(updateNode,i,num);
-            if (i<level){
-                if (updateNode.next[i]==null){
-                    updateNode.next[i] = newNode;
-                }else{
-                    Node temp = updateNode.next[i];
-                    updateNode.next[i] = newNode;
-                    newNode.next[i] = temp;
+    public void insert(int value) {
+        int level = head.forwards[0] == null ? 1 : randomLevel();
+        // 每次只增加一层，如果条件满足
+        if (level > levelCount) {
+            level = ++levelCount;
+        }
+        Node newNode = new Node(level);
+        newNode.data = value;
+        Node update[] = new Node[level];
+        for (int i = 0; i < level; ++i) {
+            update[i] = head;
+        }
+
+        Node p = head;
+        // 从最大层开始查找，找到前一节点，通过--i，移动到下层再开始查找
+        for (int i = levelCount - 1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                // 找到前一节点
+                p = p.forwards[i];
+            }
+            // levelCount 会 > level，所以加上判断
+            if (level > i) {
+                update[i] = p;
+            }
+
+        }
+        for (int i = 0; i < level; ++i) {
+            newNode.forwards[i] = update[i].forwards[i];
+            update[i].forwards[i] = newNode;
+        }
+
+    }
+
+
+    /**
+     * 删除元素
+     * @param value
+     */
+    public void delete(int value) {
+        Node[] update = new Node[levelCount];
+        Node p = head;
+        for (int i = levelCount - 1; i >= 0; --i) {
+            while (p.forwards[i] != null && p.forwards[i].data < value) {
+                p = p.forwards[i];
+            }
+            update[i] = p;
+        }
+
+        if (p.forwards[0] != null && p.forwards[0].data == value) {
+            for (int i = levelCount - 1; i >= 0; --i) {
+                if (update[i].forwards[i] != null && update[i].forwards[i].data == value) {
+                    update[i].forwards[i] = update[i].forwards[i].forwards[i];
                 }
             }
         }
-        /**
-         * 如果随机出来的层数比当前的层数还大，那么超过currentLevel的head 直接指向newNode
-         */
-        if (level > currentLevel){
-            for (int i = currentLevel; i < level; i++) {
-                head.next[i] = newNode;
-            }
-            currentLevel = level;
-        }
-
-    }
-
-    public boolean erase(int num) {
-        boolean flag = false;
-        Node searchNode = head;
-        for (int i = currentLevel-1; i >=0; i--) {
-            searchNode = findClosest(searchNode, i, num);
-            if (searchNode.next[i]!=null && searchNode.next[i].value == num){
-                //找到该层中该节点
-                searchNode.next[i] = searchNode.next[i].next[i];
-                flag = true;
-                continue;
-            }
-        }
-        return flag;
     }
 
     /**
-     * 找到level层 value 大于node 的节点
-     * @param node
-     * @param levelIndex
-     * @param value
+     * 随机 level 次，如果是奇数层数 +1，防止伪随机
+     *
      * @return
      */
-    private Node findClosest(Node node,int levelIndex,int value){
-        while ((node.next[levelIndex])!=null && value >node.next[levelIndex].value){
-            node = node.next[levelIndex];
-        }
-        return node;
-    }
-
-
-    /**
-     * 随机一个层数
-     */
-    private static int randomLevel(){
+    private int randomLevel() {
         int level = 1;
-        while (Math.random()<DEFAULT_P_FACTOR && level<DEFAULT_MAX_LEVEL){
-            level ++ ;
+        for (int i = 1; i < MAX_LEVEL; ++i) {
+            if (r.nextInt() % 2 == 1) {
+                level++;
+            }
         }
         return level;
     }
 
 
-    class Node{
-        Integer value;
-        Node[] next;
+    /**
+     * 打印所有数据
+     */
+    public void printAll_beautiful() {
+        Node p = head;
+        Node[] c = p.forwards;
+        Node[] d = c;
+        int maxLevel = c.length;
+        for (int i = maxLevel - 1; i >= 0; i--) {
+            do {
+                System.out.print((d[i] != null ? d[i].data : null) + ":" + i + "-------");
+            } while (d[i] != null && (d = d[i].forwards)[i] != null);
+            System.out.println();
+            d = c;
+        }
+    }
 
-        public Node(Integer value,int size) {
-            this.value = value;
-            this.next = new Node[size];
+    /**
+     * 跳表的节点，每个节点记录了当前节点数据和所在层数数据
+     */
+    public class Node {
+        private int data = -1;
+        /**
+         * 表示当前节点位置的下一个节点所有层的数据，从上层切换到下层，就是数组下标-1，
+         * forwards[3]表示当前节点在第三层的下一个节点。
+         */
+        private Node forwards[];
+
+
+        public Node(int level) {
+            forwards = new Node[level];
         }
 
         @Override
         public String toString() {
-            return String.valueOf(value);
+            StringBuilder builder = new StringBuilder();
+            builder.append("{ data: ");
+            builder.append(data);
+            builder.append(" }");
+            return builder.toString();
         }
     }
+
+    public static void main(String[] args) {
+        SkipList list2 = new SkipList();
+        list2.insert(1);
+        list2.insert(2);
+        list2.insert(6);
+        list2.insert(7);
+        list2.insert(8);
+        list2.insert(3);
+        list2.insert(4);
+        list2.insert(5);
+        System.out.println();
+        list2.printAll_beautiful();
+    }
+
 
 }

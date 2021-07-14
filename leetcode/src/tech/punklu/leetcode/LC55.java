a6540e5f3bbab54fd0f3b00ad144826c4f1199ae
punk1u <punk1u@protonmail.com>
2021-07-14 12:10:32 +0800

    commit leetcode55 solution

---
+++ leetcode/src/tech/punklu/leetcode/LC55.java
@@ -0,0 +1,61 @@
+package tech.punklu.leetcode;
+
+/**
+ *给定一个非负整数数组nums ，你最初位于数组的 第一个下标 。
+ *
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * 判断你是否能够到达最后一个下标。
+ *
+ *
+ * 示例1：
+ * 输入：nums = [2,3,1,1,4]
+ * 输出：true
+ * 解释：可以先跳 1 步，从下标 0 到达下标 1, 然后再从下标 1 跳 3 步到达最后一个下标。
+ *
+ * 示例2：
+ * 输入：nums = [3,2,1,0,4]
+ * 输出：false
+ * 解释：无论怎样，总会到达下标为 3 的位置。但该下标的最大跳跃长度是 0 ， 所以永远不可能到达最后一个下标。
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * 0 <= nums[i] <= 105
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/jump-game
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LC55 {
+
+    /**
+     * 运用贪心思想，从第一个元素开始向后遍历，
+     * 一直拿当前位置的元素值nums[i] + i和之前的最大跳跃位置进行比较，
+     * 如果当前的最大跳跃位置比之前的大，说明应该使用现在的最大跳跃位置，
+     * 不断向后遍历，如果出现了一个位置的最大跳跃位置(nums[i]+i)大于等于最后
+     * 一个元素的位置nums.length-1，说明能够达到最后一个下标
+     * @param nums
+     * @return
+     */
+    public static boolean canJump(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        //覆盖范围
+        int coverRange = nums[0];
+        //在覆盖范围内更新最大的覆盖范围
+        for (int i = 0; i <= coverRange; i++) {
+            coverRange = Math.max(coverRange, i + nums[i]);
+            if (coverRange >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        System.out.println(canJump(nums));
+    }
+}
